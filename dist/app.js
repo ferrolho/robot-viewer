24108ef5ed4d7986fc67bc018bbe1f0a87464eb5
Henrique Ferrolho <henriqueferrolho@gmail.com>
2017-09-21 00:24:17 +0100

    Minor updates to robots list

--- dist/app.js
+++ dist/app.js
@@ -46651,7 +46651,7 @@ function loadModel(model) {
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
-var ColladaRobotsList = ['abb_irb1200_5_90', 'abb_irb120_3_58', 'abb_irb1600_6_12', 'abb_irb2400', 'abb_irb2600_12_165', 'abb_irb4400l_30_243', 'abb_irb4600_60_205', 'abb_irb52_7_120', 'abb_irb5400', 'abb_irb6640_185_280', 'abb_irb6640', 'abb_irb7600_150_350', 'kawada_hironx', 'kuka_kr10r1100sixx', 'kuka_kr120r2500pro', 'kuka_kr16_2', 'kuka_kr5_arc', 'kuka_lbr_iiwa_14_r820', 'universal_robot_ur10', 'universal_robot_ur3', 'universal_robot_ur5'];
+var ColladaRobotsList = ['abb_irb52_7_120', 'abb_irb120_3_58', 'abb_irb1200_5_90', 'abb_irb1600_6_12', 'abb_irb2400', 'abb_irb2600_12_165', 'abb_irb4400l_30_243', 'abb_irb4600_60_205', 'abb_irb5400', 'abb_irb6640', 'abb_irb6640_185_280', 'abb_irb7600_150_350', 'kawada_hironx', 'kuka_kr5_arc', 'kuka_kr10r1100sixx', 'kuka_kr16_2', 'kuka_kr120r2500pro', 'kuka_lbr_iiwa_14_r820', 'nasa_valkyrie', 'universal_robot_ur3', 'universal_robot_ur5', 'universal_robot_ur10'];
 
 if ((typeof module === 'undefined' ? 'undefined' : _typeof(module)) === 'object') {
   module.exports = ColladaRobotsList;
