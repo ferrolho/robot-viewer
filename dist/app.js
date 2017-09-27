39a824f9785d72ff84028ad0b259eac449bf34b8
Henrique Ferrolho <henriqueferrolho@gmail.com>
2017-09-27 01:50:03 +0100

    Forgotten fix from merge

--- dist/app.js
+++ dist/app.js
@@ -63482,19 +63482,16 @@ var tweenParameters = {};
 var modelsInScene = [];
 
 async function addCollada(collada) {
-  var dae = collada.scene;
+  dae = collada.scene;
 
   dae.traverse(function (child) {
     if (child instanceof THREE.Mesh) {
       // model does not have normals
       child.material.flatShading = true;
-
-      if (castShadows) {
-        child.castShadow = true;
-        child.receiveShadow = true;
-      }
     }
   });
+
+  updateShadowsState();
 
   dae.scale.x = dae.scale.y = dae.scale.z = 5.0;
   dae.updateMatrix();
