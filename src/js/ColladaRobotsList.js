2c699508c8a0cc5752c981e45454828065c02bc8
Henrique Ferrolho <henriqueferrolho@gmail.com>
2017-11-02 23:48:30 +0000

    Adds KUKA and UR robots data info

--- src/js/ColladaRobotsList.js
+++ src/js/ColladaRobotsList.js
@@ -10,23 +10,23 @@ var ColladaRobotsList = [
   { brand: 'ABB', name: 'IRB 4600-60/2.05', id: 'abb_irb4600_60_205',  reach: 2.05,  weight:  425, payload:  60, dof: 6, tipLinks: ['tool0'], dataSheet: 'http://search.abb.com/library/Download.aspx?DocumentID=ROB0109EN_G&LanguageCode=en&DocumentPartId=&Action=Launch' },
   { brand: 'ABB', name: 'IRB 5400',         id: 'abb_irb5400',         reach: 3.129, weight:  970, payload:  25, dof: 6, tipLinks: ['tool0'], dataSheet: 'http://search.abb.com/library/Download.aspx?DocumentID=PR10269EN&LanguageCode=en&DocumentPartId=&Action=Launch' },
   { brand: 'ABB', name: 'IRB 6640-235',     id: 'abb_irb6640',         reach: 2.55,  weight: 1310, payload: 235, dof: 6, tipLinks: ['tool0'], dataSheet: 'http://search.abb.com/library/Download.aspx?DocumentID=ROB0001EN&LanguageCode=en&DocumentPartId=&Action=Launch' },
-  { brand: 'ABB', name: 'IRB 7600-150/3.5', id: 'abb_irb7600_150_350', reach: 3.50,  weight: 2450, payload: 150, dof: 6, tipLinks: ['tool0'], dataSheet: 'http://search.abb.com/library/Download.aspx?DocumentID=PR10074EN_R10&LanguageCode=en&DocumentPartId=&Action=Launch' },
+  { brand: 'ABB', name: 'IRB 7600-150/3.5', id: 'abb_irb7600_150_350', reach: 3.5,   weight: 2450, payload: 150, dof: 6, tipLinks: ['tool0'], dataSheet: 'http://search.abb.com/library/Download.aspx?DocumentID=PR10074EN_R10&LanguageCode=en&DocumentPartId=&Action=Launch' },
 
   { brand: 'Clearpath', name: 'Dual Arm Husky', id: 'clearpath_dual_arm_husky', tipLinks: [] },
 
   { brand: 'KAWADA', name: 'HiroNX', id: 'kawada_hironx', tipLinks: ['LHAND_JOINT0_Link', 'RHAND_JOINT0_Link'] },
 
-  { brand: 'KUKA', name: 'KR 5 arc',         id: 'kuka_kr5_arc',          tipLinks: ['tool0'] },
-  { brand: 'KUKA', name: 'KR 10 R1100 sixx', id: 'kuka_kr10r1100sixx',    tipLinks: ['tool0'] },
-  { brand: 'KUKA', name: 'KR 16-2',          id: 'kuka_kr16_2',           tipLinks: ['tool0'] },
-  { brand: 'KUKA', name: 'KR 120 R2500 pro', id: 'kuka_kr120r2500pro',    tipLinks: ['tool0'] },
-  { brand: 'KUKA', name: 'LBR iiwa 14 R820', id: 'kuka_lbr_iiwa_14_r820', tipLinks: ['tool0'] },
+  { brand: 'KUKA', name: 'KR 5 arc',         id: 'kuka_kr5_arc',          tipLinks: ['tool0'], payload:   5, reach: 1.412, dof: 6, weight:  127 },
+  { brand: 'KUKA', name: 'KR 10 R1100 sixx', id: 'kuka_kr10r1100sixx',    tipLinks: ['tool0'], payload:  10, reach: 1.101, dof: 6, weight:   55, productWebPage: 'https://www.kuka.com/en-gb/products/robotics-systems/industrial-robots/kr-agilus' },
+  { brand: 'KUKA', name: 'KR 16-2',          id: 'kuka_kr16_2',           tipLinks: ['tool0'], payload:  16, reach: 1.611, dof: 6, weight:  235, productWebPage: 'https://www.kuka.com/en-gb/products/robotics-systems/industrial-robots/kr-16' },
+  { brand: 'KUKA', name: 'KR 120 R2500 pro', id: 'kuka_kr120r2500pro',    tipLinks: ['tool0'], payload: 120, reach: 2.496, dof: 6, weight: 1049, productWebPage: 'https://www.kuka.com/en-gb/products/robotics-systems/industrial-robots/kr-quantec-pro' },
+  { brand: 'KUKA', name: 'LBR iiwa 14 R820', id: 'kuka_lbr_iiwa_14_r820', tipLinks: ['tool0'], payload:  14, reach: 0.82,  dof: 7, weight:   30, productWebPage: 'https://www.kuka.com/en-gb/products/robotics-systems/industrial-robots/lbr-iiwa' },
 
-  { brand: 'NASA', name: 'Valkyrie', id: 'nasa_valkyrie', tipLinks: ['leftPalm', 'rightPalm', 'leftFoot', 'rightFoot'] },
+  { brand: 'NASA', name: 'Valkyrie', id: 'nasa_valkyrie', tipLinks: ['leftPalm', 'rightPalm', 'leftFoot', 'rightFoot'], dof: 44, weight: 136 },
 
-  { brand: 'Universal', name: 'UR3',  id: 'universal_robot_ur3',  tipLinks: ['tool0'] },
-  { brand: 'Universal', name: 'UR5',  id: 'universal_robot_ur5',  tipLinks: ['tool0'] },
-  { brand: 'Universal', name: 'UR10', id: 'universal_robot_ur10', tipLinks: ['tool0'] },
+  { brand: 'Universal Robots', name: 'UR3',  id: 'universal_robot_ur3',  tipLinks: ['tool0'], payload:  3, reach: 0.5,  dof: 6, weight: 15, dataSheet: 'https://www.universal-robots.com/media/1514546/101081_199901_ur3_technical_details_web_a4_art03_rls_eng.pdf',  productWebPage: 'https://www.universal-robots.com/products/ur3-robot/' },
+  { brand: 'Universal Robots', name: 'UR5',  id: 'universal_robot_ur5',  tipLinks: ['tool0'], payload:  5, reach: 0.85, dof: 6, weight: 15, dataSheet: 'https://www.universal-robots.com/media/1514597/101081_199901_ur5_technical_details_web_a4_art03_rls_eng.pdf',  productWebPage: 'https://www.universal-robots.com/products/ur5-robot/' },
+  { brand: 'Universal Robots', name: 'UR10', id: 'universal_robot_ur10', tipLinks: ['tool0'], payload: 10, reach: 1.3,  dof: 6, weight: 17, dataSheet: 'https://www.universal-robots.com/media/1514642/101081_199901_ur10_technical_details_web_a4_art03_rls_eng.pdf', productWebPage: 'https://www.universal-robots.com/products/ur10-robot/' },
 
 ]
 
